674f3263a8225d4ccd2e643f7258d09e62fcd5c0
Nama Anda <email@ar.rifqi2006.com>
2025-12-31 08:08:43 +0700

    Update himpunan.go

--- himpunan/himpunan.go
+++ himpunan/himpunan.go
@@ -29,33 +29,3 @@ func operasiHimpunan(A, B, C []int) []int {
 			result = append(result, b)
 		}
 	}
-
-	final := []int{}
-	for _, x := range result {
-		found := false
-		for _, c := range C {
-			if x == c {
-				found = true
-			}
-		}
-		if !found {
-			final = append(final, x)
-		}
-	}
-	return final
-}
-
-// Soal 2
-func pasanganSubset(S []int, K int) {
-	fmt.Println("Pasangan (x,y) dengan x+y >", K)
-	total := 0
-	for i := 0; i < len(S); i++ {
-		for j := i + 1; j < len(S); j++ {
-			if S[i]+S[j] > K {
-				fmt.Println(S[i], S[j])
-				total++
-			}
-		}
-	}
-	fmt.Println("Total pasangan:", total)
-}
